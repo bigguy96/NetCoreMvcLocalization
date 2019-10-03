8bda6f3f723f64a6dd152e590b53fc128d45181b
Gaetan Tauvette <gaetan.tauvette@tc.gc.ca>
2019-10-03 17:16:18 -0400

    Code cleanup and added a template.

--- CoreLocalizationTest/Controllers/TestController.cs
+++ CoreLocalizationTest/Controllers/TestController.cs
@@ -1,25 +1,13 @@
-﻿using System.Globalization;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.Extensions.Localization;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace CoreLocalizationTest.Controllers
 {
     public class TestController : Controller
     {
-        private readonly IStringLocalizer<SharedResource> _localizer;
-
-        public TestController(IStringLocalizer<SharedResource> localizer)
-        {
-            _localizer = localizer;
-        }
-
         [HttpGet("/{culture:regex(en)}/test/about")]
         [HttpGet("/{culture:regex(fr)}/tester/apropos")]
         public IActionResult About()
         {
-            var q = _localizer["Hello"];
-
             return View();
         }
 
@@ -27,40 +15,7 @@ namespace CoreLocalizationTest.Controllers
         {
             return View();
         }
-
-        //public override void OnActionExecuting(ActionExecutingContext context)
-        //{
-        //    var culture = context.RouteData.Values["culture"].ToString();
-
-        //    switch (culture.ToLower())
-        //    {
-        //        case "eng":
-        //            {
-        //                SetCulture("en-CA");
-
-        //                break;
-        //            }
-        //        case "fra":
-        //            {
-        //                SetCulture("fr-CA");
-
-        //                break;
-        //            }
-        //        default:
-        //            {
-        //                SetCulture("en-CA");
-
-        //                break;
-        //            }
-        //    }
-
-        //    base.OnActionExecuting(context);
-        //}
-
-        //private void SetCulture(string lang)
-        //{
-        //    CultureInfo.CurrentCulture = new CultureInfo(lang);
-        //    CultureInfo.CurrentUICulture = new CultureInfo(lang);
-        //}
     }
 }
+
+//https://ssl-templates.services.gc.ca/app/cls/WET/gcintranet/v4_0_31/cdts/appTop/apptop_nomenu-en.shtml
