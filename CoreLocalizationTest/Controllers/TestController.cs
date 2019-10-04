67c2aed724e08aa734c5eea239ea60a1e0593ce6
Gaetan Tauvette <gaetan.tauvette@tc.gc.ca>
2019-10-04 11:19:22 -0400

    got the toggle links working.

--- CoreLocalizationTest/Controllers/TestController.cs
+++ CoreLocalizationTest/Controllers/TestController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace CoreLocalizationTest.Controllers
 {
@@ -14,6 +18,30 @@ namespace CoreLocalizationTest.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var culture = context.RouteData.Values["culture"].ToString();
+            var alternateCulture = culture.Equals("en") ? "fr" : "en";
+            var path = string.Empty;
+
+            if (context.ActionDescriptor is ControllerActionDescriptor controllerActionDescriptor)
+            {
+                var actionAttributes = controllerActionDescriptor.MethodInfo.GetCustomAttributes(inherit: true)
+                    .Cast<HttpGetAttribute>()
+                    .SingleOrDefault(x => !x.Template.Contains(culture));
+
+                var regex = new Regex(@"/{(.*?)}");
+                if (actionAttributes != null)
+                {
+                    path = regex.Replace(actionAttributes.Template, "");
+                }
+            }
+
+            ViewData["Toggle"] = $"/{alternateCulture}{path}";
+
+            base.OnActionExecuting(context);
         }
     }
 }
